85be1e8c6193259784d21089730cfd0b91a88637
huanggab <huanggab@google.com>
2019-10-08 13:47:07 -0700

    add processing script for youtube and recipes

---
+++ MASS-unsupNMT/process-youtube-recipes.sh
@@ -0,0 +1,192 @@
+#!/bin/bash
+# Copyright (c) 2019-present, Facebook, Inc.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# NOTICE FILE in the root directory of this source tree.
+#
+
+set -e
+
+
+# Data preprocessing configuration
+#
+N_MONO=5000000  # number of monolingual sentences for each language
+CODES=60000     # number of BPE codes
+N_THREADS=16    # number of threads in data preprocessing
+
+SRC="recipe1m"
+TGT="youtube8m"
+
+
+#
+# Read arguments
+#
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
+case $key in
+  --reload_codes)
+    RELOAD_CODES="$2"; shift 2;;
+  --reload_vocab)
+    RELOAD_VOCAB="$2"; shift 2;;
+  --install)
+    INSTALL=1; shift 1;;
+  *)
+  POSITIONAL+=("$1")
+  shift
+  ;;
+esac
+done
+set -- "${POSITIONAL[@]}"
+
+
+#
+# Check parameters
+#
+if [ "$SRC" == "" ]; then echo "--src not provided"; exit; fi
+if [ "$TGT" == "" ]; then echo "--tgt not provided"; exit; fi
+if [ "$SRC" != "youtube8m" -a "$SRC" != "recipe1m" ]; then echo "unknown source language"; exit; fi
+if [ "$TGT" != "youtube8m" -a "$TGT" != "recipe1m" ]; then echo "unknown target language"; exit; fi
+if [ "$SRC" == "$TGT" ]; then echo "source and target cannot be identical"; exit; fi
+if [ "$SRC" \> "$TGT" ]; then echo "please ensure SRC < TGT"; exit; fi
+if [ "$RELOAD_CODES" != "" ] && [ ! -f "$RELOAD_CODES" ]; then echo "cannot locate BPE codes"; exit; fi
+if [ "$RELOAD_VOCAB" != "" ] && [ ! -f "$RELOAD_VOCAB" ]; then echo "cannot locate vocabulary"; exit; fi
+if [ "$RELOAD_CODES" == "" -a "$RELOAD_VOCAB" != "" -o "$RELOAD_CODES" != "" -a "$RELOAD_VOCAB" == "" ]; then echo "BPE codes should be provided if and only if vocabulary is also provided"; exit; fi
+
+
+#
+# Initialize tools and data paths
+#
+
+# main paths
+MAIN_PATH=$PWD
+TOOLS_PATH=$PWD/tools
+DATA_PATH=$PWD/data
+MONO_PATH=$DATA_PATH/mono
+PARA_PATH=$DATA_PATH/para
+PROC_PATH=$DATA_PATH/processed/$SRC-$TGT
+
+# create paths
+mkdir -p $TOOLS_PATH
+mkdir -p $DATA_PATH
+mkdir -p $MONO_PATH
+mkdir -p $PARA_PATH
+mkdir -p $PROC_PATH
+
+
+# fastBPE
+FASTBPE_DIR=$TOOLS_PATH/fastBPE
+FASTBPE=$TOOLS_PATH/fastBPE/fast
+
+# raw and tokenized files
+SRC_RAW=$MONO_PATH/$SRC/all.$SRC
+TGT_RAW=$MONO_PATH/$TGT/all.$TGT
+SRC_TOK=$SRC_RAW.tok
+TGT_TOK=$TGT_RAW.tok
+
+# BPE / vocab files
+BPE_CODES=$PROC_PATH/codes
+SRC_VOCAB=$PROC_PATH/vocab.$SRC
+TGT_VOCAB=$PROC_PATH/vocab.$TGT
+FULL_VOCAB=$PROC_PATH/vocab.$SRC-$TGT
+
+# train / valid / test monolingual BPE data
+SRC_TRAIN_BPE=$PROC_PATH/train.$SRC
+TGT_TRAIN_BPE=$PROC_PATH/train.$TGT
+SRC_VALID_BPE=$PROC_PATH/valid.$SRC
+TGT_VALID_BPE=$PROC_PATH/valid.$TGT
+SRC_TEST_BPE=$PROC_PATH/test.$SRC
+TGT_TEST_BPE=$PROC_PATH/test.$TGT
+
+# valid / test parallel BPE data
+PARA_SRC_VALID_BPE=$PROC_PATH/valid.$SRC-$TGT.$SRC
+PARA_TGT_VALID_BPE=$PROC_PATH/valid.$SRC-$TGT.$TGT
+PARA_SRC_TEST_BPE=$PROC_PATH/test.$SRC-$TGT.$SRC
+PARA_TGT_TEST_BPE=$PROC_PATH/test.$SRC-$TGT.$TGT
+
+
+######### ACTUAL CODE ###################
+
+
+echo "Copying pretokenized files to .tok"
+cp "$MONO_PATH/recipe1m/Recipe1M_steps.txt" $SRC_TOK
+cp "$MONO_PATH/youtube8m/YouTube8M_asr.txt" $TGT_TOK
+
+# reload BPE codes
+cd $MAIN_PATH
+if [ ! -f "$BPE_CODES" ] && [ -f "$RELOAD_CODES" ]; then
+  echo "Reloading BPE codes from $RELOAD_CODES ..."
+  cp -v $RELOAD_CODES $BPE_CODES
+fi
+
+echo "reload codes $RELOAD_CODES bpe codes $BPE_CODES "
+
+# learn BPE codes
+if [ ! -f "$BPE_CODES" ]; then
+  echo "Learning BPE codes..."
+  $FASTBPE learnbpe $CODES $SRC_TOK $TGT_TOK > $BPE_CODES
+fi
+echo "BPE learned in $BPE_CODES"
+
+
+
+# apply BPE codes
+if ! [[ -f "$SRC_TRAIN_BPE" ]]; then
+  echo "Applying $SRC BPE codes..."
+  $FASTBPE applybpe $SRC_TRAIN_BPE $SRC_TOK $BPE_CODES
+fi
+if ! [[ -f "$TGT_TRAIN_BPE" ]]; then
+  echo "Applying $TGT BPE codes..."
+  $FASTBPE applybpe $TGT_TRAIN_BPE $TGT_TOK $BPE_CODES
+fi
+echo "BPE codes applied to $SRC in: $SRC_TRAIN_BPE"
+echo "BPE codes applied to $TGT in: $TGT_TRAIN_BPE"
+
+# extract source and target vocabulary
+if ! [[ -f "$SRC_VOCAB" && -f "$TGT_VOCAB" ]]; then
+  echo "Extracting vocabulary..."
+  $FASTBPE getvocab $SRC_TRAIN_BPE > $SRC_VOCAB
+  $FASTBPE getvocab $TGT_TRAIN_BPE > $TGT_VOCAB
+fi
+echo "$SRC vocab in: $SRC_VOCAB"
+echo "$TGT vocab in: $TGT_VOCAB"
+
+# reload full vocabulary
+cd $MAIN_PATH
+if [ ! -f "$FULL_VOCAB" ] && [ -f "$RELOAD_VOCAB" ]; then
+  echo "Reloading vocabulary from $RELOAD_VOCAB ..."
+  cp $RELOAD_VOCAB $FULL_VOCAB
+fi
+
+# extract full vocabulary
+if ! [[ -f "$FULL_VOCAB" ]]; then
+  echo "Extracting vocabulary..."
+  $FASTBPE getvocab $SRC_TRAIN_BPE $TGT_TRAIN_BPE > $FULL_VOCAB
+fi
+echo "Full vocab in: $FULL_VOCAB"
+
+# binarize data
+if ! [[ -f "$SRC_TRAIN_BPE.pth" ]]; then
+  echo "Binarizing $SRC data..."
+  $MAIN_PATH/preprocess.py $FULL_VOCAB $SRC_TRAIN_BPE
+fi
+if ! [[ -f "$TGT_TRAIN_BPE.pth" ]]; then
+  echo "Binarizing $TGT data..."
+  $MAIN_PATH/preprocess.py $FULL_VOCAB $TGT_TRAIN_BPE
+fi
+echo "$SRC binarized data in: $SRC_TRAIN_BPE.pth"
+echo "$TGT binarized data in: $TGT_TRAIN_BPE.pth"
+
+
+
+
+#
+# Summary
+#
+echo ""
+echo "===== Data summary"
+echo "Monolingual training data:"
+echo "    $SRC: $SRC_TRAIN_BPE.pth"
+echo "    $TGT: $TGT_TRAIN_BPE.pth"
